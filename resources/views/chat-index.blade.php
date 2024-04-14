9c7507444d34da8a63e28748fd03d9508fb9feb8
Roy Flaherty <royfla100@gmail.com>
2024-04-14 15:17:39 +0100

    start chat styling

--- resources/views/chat-index.blade.php
+++ resources/views/chat-index.blade.php
@@ -2,24 +2,20 @@
 
     <x-slot:title>Chat</x-slot>
 
-    <main class="container p-3">
-        <div class="row ms-2 me-2 mb-2">
-            <div class="col-12 col-md-3">
-                Hello, {{ Auth::user()->first_name }}
-            </div>
-        </div>
+    <main class="fluid-container">
         <div class="row">
-            <div class="col-12 col-md-3">
+            <div class="col-12 col-md-5 sideform p-4">
                 <?php
                 // // need to update this so it also shows if I sent the only message
                 // $statement = $pdo->prepare('SELECT DISTINCT users.username, users.id,  last_activity, (select count(*) from messages where sender_id = users.id and receiver_id = :me and opened = 0) AS unread_count FROM messages JOIN users ON users.id = messages.sender_id WHERE receiver_id = :me');
                 ?>
+                <h3>Your Sparks:</h3>
                 <x-chat-user-list></x-chat-user-list>
             </div>
-            <div class="col-9 d-none d-md-block text-center d-flex justify-content-center flex-column">
-                <i class="bi-chat-dots chat-icon"></i>
-                <h1>Welcome to Spark Chat!</h1>
-                <p>Click on a name to get started.</p>
+            <div class="col-7 d-none text-center d-md-flex justify-content-center align-items-center  bg-white flex-column">
+                <i class="bi-chat-dots chat-icon colored-text"></i>
+                <h1 class="colored-text">Welcome to Spark Chat, {{ Auth::user()->first_name }}!</h1>
+                <p class="colored-text">Click on a name to get started.</p>
             </div>
         </div>
     </main>
@@ -28,5 +24,9 @@
 <style>
     .chat-icon {
         font-size: 4.5em;
+        
+    }
+    .colored-text {
+        color: var(--spk-color-primary-2);
     }
 </style>
