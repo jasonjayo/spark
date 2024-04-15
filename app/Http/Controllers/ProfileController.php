a7d22b0fb0eda0fee93f718e072c2f17b0477780
Roy Flaherty <royfla100@gmail.com>
2024-04-15 14:20:04 +0100

    exclude user from search listings

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -34,7 +34,7 @@ class ProfileController extends Controller
         $query = Profile::filter(request(['min_age', 'max_age', 'gender', 'online_now', 'interests', 'max_distance', 'query']));
         $sql = $query->toRawSQL();
         return view("search", [
-            "profiles" => $query->get(),
+            "profiles" => $query->get()->except(Auth::user()->id),
             "sql" => $sql,
             "errors" => $errors
         ]);
